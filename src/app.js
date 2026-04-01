1d68807222bff59dcf43e26f42ee7a0517f789a1
Yan Kazymyr <kazymyr.yan@gmail.com>
2026-04-01 19:33:30 +0300

    Fix

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ function moveFiles() {
   const [source, destination] = process.argv.slice(2);
 
   try {
+    if (!source || !destination) {
+      throw new Error('Invzlid arguments');
+    }
+
+    if (!fs.existsSync(source) || !fs.statSync(source).isFile()) {
+      throw new Error('Source is not a file');
+    }
+
     let finalDest = destination;
 
     if (destination.endsWith('/')) {
@@ -16,13 +24,10 @@ function moveFiles() {
       }
 
       finalDest = path.join(destination, path.basename(source));
-
-      fs.renameSync(source, finalDest);
-
-      return;
-    }
-
-    if (fs.existsSync(destination) && fs.statSync(destination).isDirectory()) {
+    } else if (
+      fs.existsSync(destination) &&
+      fs.statSync(destination).isDirectory()
+    ) {
       finalDest = path.join(destination, path.basename(source));
     }
 
