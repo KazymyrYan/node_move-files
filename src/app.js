af48487296805bdb8330602b2b844e68adeeaf12
Yan Kazymyr <kazymyr.yan@gmail.com>
2026-04-01 19:22:35 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-// write code here
+const fs = require('fs');
+const path = require('path');
+
+function moveFiles() {
+  const [source, destination] = process.argv.slice(2);
+
+  try {
+    let finalDest = destination;
+
+    if (destination.endsWith('/')) {
+      if (
+        !fs.existsSync(destination) ||
+        !fs.statSync(destination).isDirectory()
+      ) {
+        throw new Error('Destination directory does not exist');
+      }
+
+      finalDest = path.join(destination, path.basename(source));
+
+      fs.renameSync(source, finalDest);
+
+      return;
+    }
+
+    if (fs.existsSync(destination) && fs.statSync(destination).isDirectory()) {
+      finalDest = path.join(destination, path.basename(source));
+    }
+
+    fs.renameSync(source, finalDest);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error.message);
+  }
+}
+
+moveFiles();
